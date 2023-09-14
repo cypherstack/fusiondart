bf6a387f616ddef39cc543e50a2230d86e899954
Josh Babb <sneurlax@gmail.com>
2023-09-14 16:06:56 -0500

    documentation

--- lib/fusiondart.dart
+++ lib/fusiondart.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// CashFusion Dart library.
 library;
 
 export 'src/fusion.dart';
-
-// TODO: Export any libraries intended for clients of this package.
