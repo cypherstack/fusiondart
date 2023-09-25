e5aabda7893c65d083974ab4dccd78218eff6216
sneurlax <sneurlax@gmail.com>
2023-09-25 18:45:52 -0500

    another hBytes option
    none of them work

--- lib/src/fusion.dart
+++ lib/src/fusion.dart
@@ -580,8 +580,15 @@ class Fusion {
     List<(Component, int)> components = [];
 
     // Set up Pedersen setup instance.
-    Uint8List hBytes = Uint8List.fromList(
-        [0x02, ...utf8.encode('CashFusion gives us fungibility.')]);
+    // Uint8List hBytes = Uint8List.fromList(
+    //     [0x02] + 'CashFusion gives us fungibility.'.codeUnits);
+    // Uint8List hBytes = Uint8List.fromList(
+    //     [0x02, ...utf8.encode('CashFusion gives us fungibility.')]);
+    // Uint8List hBytes = Uint8List.fromList(
+    //     [...utf8.encode('\x02CashFusion gives us fungibility.')]);
+    Uint8List prefix = Uint8List.fromList([0x02]);
+    List<int> stringBytes = utf8.encode('CashFusion gives us fungibility.');
+    Uint8List hBytes = Uint8List.fromList([...prefix, ...stringBytes]);
 
     // Use secp256k1 curve.
     ECDomainParameters params = ECDomainParameters('secp256k1');
