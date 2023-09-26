c76be2b4430f9dfe1cfe65507c86ab9e3b54e9a2
sneurlax <sneurlax@gmail.com>
2023-09-26 10:55:49 -0500

    remove redundant check

--- lib/src/fusion.dart
+++ lib/src/fusion.dart
@@ -620,10 +620,6 @@ class Fusion {
       throw Exception('HG is not a valid point on the curve');
     }
 
-    if (!Utilities.isPointOnCurve(H, params.curve)) {
-      throw Exception('H1 is not a valid point on the curve');
-    }
-
     // Set up Pedersen setup.
     PedersenSetup setup = PedersenSetup(H);
 
