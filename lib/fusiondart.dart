6cd09fe76af5e20c3bf36dcd11bf26d20140e510
julian <julian@cypherstack.com>
2023-09-28 10:41:39 -0600

    add fusion models export

--- lib/fusiondart.dart
+++ lib/fusiondart.dart
@@ -1,4 +1,9 @@
 /// CashFusion Dart library.
 library;
 
+export 'src/models/address.dart';
+export 'src/models/input.dart';
+export 'src/models/output.dart';
+export 'src/models/transaction.dart';
+
 export 'src/fusion.dart';
