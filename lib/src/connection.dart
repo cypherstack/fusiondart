3107687d58c2708684c897407464e2e54db9fa01
sneurlax <sneurlax@gmail.com>
2023-09-30 14:15:30 -0500

    try-catch proxied conn

--- lib/src/connection.dart
+++ lib/src/connection.dart
@@ -78,52 +78,61 @@ class Connection {
   }) async {
     // Before we connect to host and port, if proxyInfo is not null, we should connect to the proxy first.
     if (proxyInfo != null) {
-      /*
-      // CoPilot suggested this code!  It might actually work.  I thought SocketWrapper was
-      // for this anyways, but I was going to use socks_socket...  I should probably try this
-      // out and see if it or similar works.  Better to have less dependencies.  Anyways...
+      try {
+        /*
+        // CoPilot suggested this code!  It might actually work.  I thought SocketWrapper was
+        // for this anyways, but I was going to use socks_socket...  I should probably try this
+        // out and see if it or similar works.  Better to have less dependencies.  Anyways...
 
-      // Connect to the proxy.
-      final SocketWrapper socketwrapper =
-          SocketWrapper(proxyInfo.host.address, proxyInfo.port);
-      await socketwrapper.connect();
+        // Connect to the proxy.
+        final SocketWrapper socketwrapper =
+        SocketWrapper(proxyInfo.host.address, proxyInfo.port);
+        await socketwrapper.connect();
 
-      // Send the CONNECT message.
-      final msg = <int>[]
-        ..addAll([0x05, 0x01, 0x00])
-        ..addAll([0x03, host.length])
-        ..addAll(utf8.encode(host))
-        ..addAll([port >> 8, port & 0xff]);
-      await socketwrapper.send(msg);
+        // Send the CONNECT message.
+        final msg = <int>[]
+          ..addAll([0x05, 0x01, 0x00])
+          ..addAll([0x03, host.length])
+          ..addAll(utf8.encode(host)) // import convert.
+          ..addAll([port >> 8, port & 0xff]);
+        await socketwrapper.send(msg);
 
-      // Receive the response.
-      final response = await socketwrapper.receive(10);
-      if (response[0] != 0x05 || response[1] != 0x00) {
-        throw 'Failed to connect to proxy: ${hex.encode(response)}';
+        // Receive the response.
+        final response = await socketwrapper.receive(10);
+        if (response[0] != 0x05 || response[1] != 0x00) {
+          throw 'Failed to connect to proxy: ${hex.encode(response)}';
+        }
+
+        // Connect to the host and port through the proxy.
+        socketwrapper.connectTo(host, port); // connectTo doesn't exist.  So close, CoPilot.
+        */
+
+        // From https://github.com/cypherstack/tor/blob/53b1c97a41542956fc6887878ba3147abae20ccd/example/lib/main.dart#L166
+
+        // Instantiate a socks socket at localhost and on the port selected by the tor service.
+        var socksSocket = await SOCKSSocket.create(
+          proxyHost: proxyInfo.host.address,
+          proxyPort: proxyInfo.port,
+          sslEnabled: ssl,
+        );
+
+        // Connect to the socks instantiated above.
+        await socksSocket.connect();
+
+        // Connect to CashFusion server.
+        await socksSocket.connectTo(host, port);
+
+        return Connection(
+          socket:
+              socksSocket.socket, // This might not "just work", but it might.
+          timeout: defaultTimeout,
+        );
+        // TODO Close the socket.
+        // await socksSocket.close();
+      } catch (e, s) {
+        Utilities.debugPrint(s);
+        throw 'openConnection(): Failed to open proxied connection: $e';
       }
-      */
-
-      // From https://github.com/cypherstack/tor/blob/53b1c97a41542956fc6887878ba3147abae20ccd/example/lib/main.dart#L166
-
-      // Instantiate a socks socket at localhost and on the port selected by the tor service.
-      var socksSocket = await SOCKSSocket.create(
-        proxyHost: proxyInfo.host.address,
-        proxyPort: proxyInfo.port,
-        sslEnabled: true, // TODO is this an SSL connection?
-      );
-
-      // Connect to the socks instantiated above.
-      await socksSocket.connect();
-
-      // Connect to CashFusion server.
-      await socksSocket.connectTo(host, port);
-
-      return Connection(
-        socket: socksSocket.socket, // This might not "just work", but it might.
-        timeout: defaultTimeout,
-      );
-      // TODO Close the socket.
-      // await socksSocket.close();
     } else {
       try {
         // Connect to host and port.
@@ -142,7 +151,7 @@ class Connection {
           timeout: defaultTimeout,
         );
       } catch (e) {
-        throw 'Failed to open connection: $e';
+        throw 'openConnection(): Failed to open direct connection: $e';
       }
     }
   }
