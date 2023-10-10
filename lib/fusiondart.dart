5ad600ddd8c132f1f42599bf68106f191fe49e35
sneurlax <sneurlax@gmail.com>
2023-10-10 10:44:51 -0500

    remove unavailable export

--- lib/fusiondart.dart
+++ lib/fusiondart.dart
@@ -3,7 +3,6 @@ library;
 
 export 'src/fusion.dart';
 export 'src/models/address.dart';
-export 'src/models/input.dart';
 export 'src/models/output.dart';
 export 'src/models/transaction.dart';
 export 'src/models/utxo_dto.dart';
