b2a830695b5530d2a1673ecb5f181eeb0a0c86a3
julian <julian@cypherstack.com>
2023-10-12 12:49:20 -0600

    add equality operator override

--- lib/src/models/utxo_dto.dart
+++ lib/src/models/utxo_dto.dart
@@ -1,3 +1,5 @@
+import 'package:fusiondart/src/extensions/on_list_int.dart';
+
 class UtxoDTO {
   final String txid;
   final int vout;
@@ -12,4 +14,20 @@ class UtxoDTO {
     required this.pubKey,
     required this.address,
   }) : assert(pubKey.isNotEmpty);
+
+  @override
+  bool operator ==(other) {
+    if (other is UtxoDTO) {
+      return txid == other.txid &&
+          vout == other.vout &&
+          value == other.value &&
+          address == other.address &&
+          pubKey.equals(other.pubKey);
+    }
+
+    return false;
+  }
+
+  @override
+  int get hashCode => Object.hash(txid, vout, value, pubKey, address);
 }
